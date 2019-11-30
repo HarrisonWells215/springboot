2127e4140608021874e446c621660138c181cf3b
HarrisonWells215 <zhengweixi123@qq.com>
2019-11-30 13:13:17 +0800

    first commit

---
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * ClassName HelloController
+ **/
+@Controller//接收前端请求
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
